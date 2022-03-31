68e54b14a36c87d882a78f1d296d89de66c2b725
Amy de Buitléir <amy@nualeargais.ie>
2022-03-31 19:03:18 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# pandoc-columns
-Pandoc filter to handle columns in Markdown.
+# pandoc-linear-table
+
+I created this filter so that I would have an easy way to wrap text
+in table cells.
+
+## Usage
+
+Use this filter by adding `--filter=pandoc-linear-table` to your pandoc command.
+For example:
+
+    pandoc --filter=pandoc-linear-table myfile.md --output=myfile.pdf
+
+## Example
+
+    ```{.linear-table}
+    Each line of text in your markdown becomes a cell in the output table.
+    The text will wrap automatically as needed to fit the table.
+    Use a blank line to indicate the end of a row.
+
+    ` `
+    To leave a column blank, use some sort of hard space. See the previous cell for an example.
+    I call these "linear tables" because it kind of looks like a table if you look at it sideways. In hindsight, I should have called them sideways tables.
+
+    You can include other markup. Here's an [example of a link](https://github.com/mhwombat/pandoc-linear-table).
+    You can include LaTex formulas. For example: $(\bigcap_{x \in \mathscr{C}} X)'$
+    That's about all there is to know.
+    ```
+
+Depending on your output format, the result of the above will look
+like this:
+
+![](table.png)
---
+++ src/Text/Pandoc/Filters/Columns.hs
@@ -0,0 +1,58 @@
+{-|
+Module      : Columns
+Description : Pandoc filter to support columns in Markdown.
+Copyright   : (c) Amy de Buitléir, 2020-2022
+License     : BSD--3
+Maintainer  : amy@nualeargais.ie
+Stability   : experimental
+Portability : POSIX
+
+See <https://github.com/mhwombat/pandoc-linear-table> for information
+on how to use this filter.
+-}
+
+{-# LANGUAGE OverloadedStrings #-}
+module Text.Pandoc.Filters.Columns
+  (
+    transform,
+    formatColumns
+  ) where
+
+import qualified Text.Pandoc      as P
+import           Text.Pandoc.Walk (walk)
+
+
+-- | A transformation that can be used with Hakyll.
+transform :: P.Pandoc -> P.Pandoc
+transform = walk formatColumns
+
+formatColumns :: P.Block -> P.Block
+formatColumns (P.Div attr@(_,["columns"],_) bs)
+  = P.Table attr defaultTableCaption colSpecs
+                    defaultTableHeader [body]
+                    defaultTableFooter
+  where body = blocksToTableBody bs
+        colSpecs = map (\_ -> defaultColSpec) bs
+formatColumns b = b
+
+blocksToTableBody :: [P.Block] -> P.TableBody
+blocksToTableBody bs = P.TableBody P.nullAttr (P.RowHeadColumns 0) [] [row]
+  where row = blocksToTableRow bs
+
+blocksToTableRow :: [P.Block] -> P.Row
+blocksToTableRow = P.Row P.nullAttr . map blockToCell
+
+blockToCell :: P.Block -> P.Cell
+blockToCell b = P.Cell P.nullAttr P.AlignDefault (P.RowSpan 1) (P.ColSpan 1) [b]
+
+defaultColSpec :: P.ColSpec
+defaultColSpec = (P.AlignDefault, P.ColWidthDefault)
+
+defaultTableCaption :: P.Caption
+defaultTableCaption = P.Caption Nothing []
+
+defaultTableHeader :: P.TableHead
+defaultTableHeader = P.TableHead P.nullAttr []
+
+defaultTableFooter :: P.TableFoot
+defaultTableFooter = P.TableFoot P.nullAttr []
