20c51c5452daa231ed61d0c446485d3a83789608
Amy de Buitléir <amy@nualeargais.ie>
2023-03-17 20:08:48 +0000

    package as flake

--- flake.nix
+++ flake.nix
@@ -2,26 +2,21 @@
   description = "Pandoc Markdown filter that provides a Markdown extension for columns.";
 
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      {
-        packages = rec {
-          pandoc-columns = pkgs.haskellPackages.developPackage {
-            name = "pandoc-columns";
-            root = ./.;
-          };
-          default = pandoc-columns;
-        };
-        apps = rec {
-          pandoc-columns = flake-utils.lib.mkApp { drv = self.packages.${system}.pandoc-columns; };
-          default = pandoc-columns;
-        };
-      }
-    );
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.haskell-flake.flakeModule ];
+
+      perSystem = { self', pkgs, ... }: {
+        haskellProjects.default = {};
+
+        # haskell-flake doesn't set the default package, but you can do it here.
+        packages.default = self'.packages.pandoc-columns;
+      };
+    };
 }
