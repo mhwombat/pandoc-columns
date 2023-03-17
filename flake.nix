850151d8367f43531747826f0bcfe4f14c95f450
Amy de Buitléir <amy@nualeargais.ie>
2023-03-17 18:02:46 +0000

    package as flake

--- flake.nix
+++ flake.nix
@@ -2,24 +2,26 @@
   description = "Pandoc Markdown filter that provides a Markdown extension for columns.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      nameValuePair = name: value: { inherit name value; };
-      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
-      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = f: genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-      packages = forAllSystems ({ pkgs }: {
-        default = pkgs.haskellPackages.developPackage {
-          name = "pandoc-columns";
-          root = ./.;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        packages = rec {
+          pandoc-columns = pkgs.haskellPackages.developPackage {
+            name = "pandoc-columns";
+            root = ./.;
+          };
+          default = pandoc-columns;
         };
-      });
-    };
+        apps = rec {
+          pandoc-columns = flake-utils.lib.mkApp { drv = self.packages.${system}.pandoc-columns; };
+          default = pandoc-columns;
+        };
+      }
+    );
 }
